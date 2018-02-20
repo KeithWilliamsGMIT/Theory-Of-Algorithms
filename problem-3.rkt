d2b3d28e77e6a7a4962aa6bd8b9b501756d43d3f
Keith Williams <G00324844@gmit.ie>
2018-02-20 18:58:27 +0000

    Added rcycle function for third problem.

--- problem-3.rkt
+++ problem-3.rkt
@@ -8,4 +8,18 @@
   
   (lcycle-helper (cdr l) (list (car l))))
 
+(define (rcycle l)
+  (define (rcycle-helper l n)
+    (if (null? (cdr l))
+        (cons (car l) n)
+        (rcycle-helper (cdr l) (back (car l) n))))
+
+  (define (back elem list)
+    (if (null? list)
+        (cons elem list)
+        (cons (car list) (back elem (cdr list)))))
+
+  (rcycle-helper l null))
+
 (lcycle (list 1 2 3 4 5))
+(rcycle (list 1 2 3 4 5))
