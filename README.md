2225332096b091ffdb254387b3f1ea0b973e600b
Keith Williams <G00324844@gmit.ie>
2018-03-18 14:55:36 +0000

    Added eighth problem to README and added comments.

--- README.md
+++ README.md
@@ -60,3 +60,10 @@ This repository contains work from my 4th year theory of algorithms module for c
     > (maj (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
     '(0 0 0 1 0 1 1 1)
     ```
+
+8. Write a function `chse` in Racket that takes three lists `x`, `y` and `z` of equal length and containing only 0’s and 1’s. It should return a list containing the elements of `y` in the positions where `x` is 1 and the elements of `z` otherwise. For example:
+
+    ```
+    > (chse (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
+    '(0 1 0 1 0 0 1 1)
+    ```
--- problem-8.rkt
+++ problem-8.rkt
@@ -1,5 +1,12 @@
 #lang racket
 
+; This function takes three lists x, y and z of equal length
+; and containing only 0's and 1's and returns a list
+; containing a 1 where the number of 1's in a given position
+; in x is 1 and the elements of z otherwise. This function
+; works by recursively checking each element in x and adding
+; the element from the appropriate list to a new list. This
+; list is returned.
 (define (chse x y z)
   (if (null? x)
       null
@@ -7,4 +14,5 @@
           (cons (car y) (chse (cdr x) (cdr y) (cdr z)))
           (cons (car z) (chse (cdr x) (cdr y) (cdr z))))))
 
+; Expected output - (0 1 0 1 0 0 1 1)
 (chse (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
